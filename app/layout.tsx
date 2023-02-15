e0402290c88d4eeb221f33223577edcabeb030f7
vivek <vivek15292001@gmail.com>
2023-02-15 20:22:08 +0530

    Day1-part2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,37 @@
+import { SessionProvider } from '../components/SessionProvider';
 import SideBar from '../components/SideBar';
+import { getServerSession } from 'next-auth';
 import '../styles/globals.css';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
+import Login from '../components/Login';
 
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          {/* Sidebar */}
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ): (
+              <div className = "flex">
+          {/* Sidebar */ }
           <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
             <SideBar />
           </div>
-
           <div className="bg-[#343542] flex-1">{children}</div>
         </div>
-      </body>
-    </html>
+          )}
+
+      </SessionProvider>
+    </body>
+    </html >
   )
 }
